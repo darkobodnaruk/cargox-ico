ddd028743aa87d7807c175cf1570d1c187763db2
Gasper Setinc <gasper.setinc@gmail.com>
2018-01-25 23:43:35 +0100

    updated compiler version & copy

--- contracts/Airdropper.sol
+++ contracts/Airdropper.sol
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 OmiseGO Pte Ltd
+Copyright 2018 CargiX.io Pte Ltd
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
  */
 
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.18;
 
 import "./zeppelin/contracts/ownership/Ownable.sol";
 import "./zeppelin/contracts/token/ERC20.sol";
